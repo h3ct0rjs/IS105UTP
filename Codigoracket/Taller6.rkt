bb6c695b63b7614fc7dce8e0670b9bdeac958985
Hector F. Jimenez <hfjimenez@utp.edu.co>
2015-10-23 09:55:48 -0500

    Taller6, Init.

---
+++ Codigoracket/Taller6.rkt
@@ -0,0 +1,59 @@
+;;====================================
+;Taller 6.0
+;Vectores en Dr Racket
+;feedback:hfjimenez@utp.edu.co
+;UTP.20152 ISC
+;====================================
+(display "
+============================================
+\t.:::Taller 6.0:::.
+Cadenas Dr Racket.
+feedback:hfjimenez@utp.edu.co
+\t UTP.20152 ISC
+Repositorio:https://github.com/heticor915/IS105UTP
+===============================================
+NOTA:
+" )
+
+;Para cada uno de los ejercicios planteados, debe realizar un programa en Dr Scheme con
+;la siguiente documentación:
+;a. Objetivo
+;b. Plantilla
+;c. Parámetros de entrada (precondición)
+;d. Parámetro de salida (postcondición, si lo hay),
+;e. Ejemplo
+;f. Definición del programa.
+
+;1) Hacer una función que reciba una cadena de caracteres y devuelva otra mutable y con
+;el contenido de la primera.
+
+;2) Hacer una función que reciba una cadena de caracteres y un carácter adicional. La
+;cadena se debe retornar con el carácter anexaado al final de la misma.
+
+;3) Hacer una función que reciba un entero N y devuelva una cadena de longitud N,
+;leyendo por pantalla cada uno de los N caracteres de la cadena.
+
+;4) Hacer una función que reciba una cadena de caracteres mutable y la invierta en ella
+;misma.
+
+;5) Hacer una función que reciba una cadena de caracteres y devuelva otra cadena
+;invirtiendo todos los caracteres de la primera.
+
+;6) Haga una función que reciba dos cadenas de caracteres y devuelva otra cadena con la
+;concatenación de las dos primeras.
+
+;7) Hacer una función que reciba una cadena de caracteres y cuente cuantas vocales tiene.
+
+;8) Hacer una función que reciba una cadena de caracteres y devuelva otra sin las
+;vocales.
+
+;9) Construir un programa que lea un string con solo caracteres numéricos y devuelva el
+;número que representa.
+
+;10) Hacer una función que reciba una cadena de caracteres e indique si es palíndrome.
+;Una cadena de caracteres es palíndrome si se lee igual al derecho que al revés.
+;Ejemplo: “ojo”, “abcdedcba”, “a”, “dabalearrozalazorraelabad”.11) Hacer una función boolean que reciba dos cadenas de caracteres e indique si son
+;iguales.
+
+;12) Hacer una función que reciba dos cadenas de caracteres e indique si la segunda está
+;incluida en la primera (es decir si es substring).
